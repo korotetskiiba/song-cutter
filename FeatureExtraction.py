32c6bb4f44aac0e63762b3d6bf48e6af022611ec
Aleksandr Mitenev <aleksandr.mitenev@mail.ru>
2020-07-12 15:24:53 +0300

    Add asserts Add entry point in the feature extraction module

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -7,12 +7,15 @@ from vggish_files import vggish_postprocess, vggish_slim, vggish_input, vggish_p
 class Feature_extraction:
 
     @staticmethod
-    def get_audioset_features(path_pkl, path_to_save_pkl):
-        with open(path_pkl, "rb") as handle:
+    def get_audioset_features(path_to_pkl, path_to_save_pkl):
+        assert isinstance(path_to_pkl, str) and path_to_pkl.endswith('.pkl'), "Wrong path to pkl argument"
+        assert isinstance(path_to_save_pkl, str) and path_to_save_pkl.endswith('.pkl'), "Wrong path to save pkl argument"
+
+        with open(path_to_pkl, "rb") as handle:
             data_dict_music = pickle.load(handle)
 
-        file_list = data_dict_music["files_list"]
-
+        file_list = data_dict_music.get("files_list")
+        assert file_list is not None, "files list not found in "+path_to_pkl
         # Prepare a postprocessor to munge the model embeddings.
 
         pproc = vggish_postprocess.Postprocessor(vggish_params.PCA_DUMP)
@@ -30,6 +33,8 @@ class Feature_extraction:
             # Run inference and postprocessing.
             embed_list = []
             for f in file_list:
+                assert isinstance(f, str) and f.endswith('.wav'), "Wrong path to wav in files list"
+
                 examples_batch = vggish_input.wavfile_to_examples(f)
                 [embedding_batch] = sess.run([embedding_tensor],
                                              feed_dict={features_tensor: examples_batch})
@@ -41,3 +46,9 @@ class Feature_extraction:
             data_dict_music["embedings_list"] = embed_list
             with open(path_to_save_pkl, "wb") as handle:
                 pickle.dump(data_dict_music, handle)
+
+
+if __name__ == "__main__":
+    import sys
+    assert len(sys.argv) >= 3, "wrong num of arguments"
+    Feature_extraction.get_audioset_features(sys.argv[1], sys.argv[2])
