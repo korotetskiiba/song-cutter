9bf99607d8494936d952c17de01f25b59fea0f2e
Aleksandr Mitenev <aleksandr.mitenev@mail.ru>
2020-07-23 16:03:34 +0300

    Add main

--- Pipeline/Preprocessing/YouTubeMetaExtraction.py
+++ Pipeline/Preprocessing/YouTubeMetaExtraction.py
@@ -1,4 +1,6 @@
 import pytube
+import argparse
+import csv
 
 
 class YouTubeMetaExtraction:
@@ -18,3 +20,25 @@ class YouTubeMetaExtraction:
 
     def get_html(self):
         return pytube.request.get(self.__url)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Preprocessing')
+
+    parser.add_argument('-u', type=str, action="store", dest="url", help='url')
+    parser.add_argument('-o', type=str, action="store", dest="path_to_save_csv",
+                        help='path to save the output file')
+    args = parser.parse_args()
+
+    yt = YouTubeMetaExtraction(args.url)
+
+    data = {}
+
+    data['channel'] = yt.get_channel_name()
+    data['title'] = yt.get_title()
+    data['description'] = yt.get_description().replace('\n', '').replace(';', '')
+    data['html'] = yt.get_html().replace('\n', '').replace(';', '')
+
+    with open(args.path_to_save_csv, "a", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow([data['channel'], data['title'], data['description'], data['html']])
